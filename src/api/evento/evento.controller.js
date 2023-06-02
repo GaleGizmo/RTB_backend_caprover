f307397cc475a3a783ea67ef1cf60790e3e5fe5e
Miguel <miguelaf71@hotmail.com>
2023-06-02 10:42:28 +0200

    control de campos obligatorios y manejo de errores en eventos

--- src/api/evento/evento.controller.js
+++ src/api/evento/evento.controller.js
@@ -1,5 +1,6 @@
-const  Evento  = require("./evento.model");
+const Evento = require("./evento.model");
 
+//añade un evento a la BBDD
 const setEvento = async (req, res, next) => {
   try {
     const {
@@ -15,6 +16,19 @@ const setEvento = async (req, res, next) => {
       image,
       genre,
     } = req.body;
+
+    if (
+      !title ||
+      !content ||
+      !user_creator ||
+      !site ||
+      !price ||
+      !date_start ||
+      !date_end
+    ) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
     const timestamp = new Date();
     const newEvento = new Evento({
       title,
@@ -31,13 +45,71 @@ const setEvento = async (req, res, next) => {
     });
 
     await newEvento.save().then(() => {
-      return res.status(200).json(" evento creado");
+      return res.status(200).json("evento creado con éxito");
     });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      return res.status(400).json({ message: error.message });
+    }
+    return next(error);
+  }
+};
+
+//borra un evento de la BBDD
+const deleteEvento = async (req, res, next) => {
+  try {
+    const { idEvento } = req.params;
+
+    const deletedEvento = await Evento.findByIdAndDelete(idEvento);
+
+    if (!deletedEvento) {
+        return res.status(404).json({ message: "Evento no encontrado" });
+      }
+
+    return res.status(200).json(deletedEvento);
   } catch (error) {
     return next(error);
   }
 };
 
+//actualiza un evento de la BBDD
+const updateEvento = async (req, res, next) => {
+  try {
+    const { idEvento } = req.params;
 
+    const requiredFields = [
+      "title",
+      "subtitle",
+      "content",
+      "user_creator",
+      "site",
+      "price",
+      "date_start",
+    ];
+    const missingFields = requiredFields.filter((field) => !req.body[field]);
 
-module.exports = { setEvento};
+    if (missingFields.length > 0) {
+      return res.status(400).json({
+        error: `Missing required fields: ${missingFields.join(", ")}`,
+      });
+    }
+
+    const eventoToUpdate = new Evento(req.body);
+    if (req.file) {
+      eventoToUpdate.body.image = req.file.path;
+    }
+    eventoToUpdate._id = idEvento;
+
+    const updatedEvento = await Evento.findByIdAndUpdate(
+      idEvento,
+      eventoToUpdate,
+      { new: true }
+    );
+
+    return res.status(200).json(updatedEvento);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+module.exports = { setEvento, updateEvento, deleteEvento };
