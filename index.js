462782ace35e05d47cff811ec2b7991bf52f914c
Miguel <miguelaf71@hotmail.com>
2023-06-05 13:44:19 +0200

    control de errores y rutas no existentes en index

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 require("dotenv").config();
 const express = require("express");
-const cors=require("cors")
+const cors = require("cors");
 const server = express();
 
-server.use(express.urlencoded({extended:true}))
-server.use(express.json())
-const cloudinary=require("cloudinary").v2
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-  cloud_name:process.env.CLOUD_NAME,
+  cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
-
-})
+  api_secret: process.env.API_SECRET,
+});
 const PORT = process.env.PORT;
 
-server.use(cors())
+server.use(cors());
 
-server.use(express.json())
-server.use(express.urlencoded({extended:true}))
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
-const eventoRoutes=require("./src/api/evento/evento.routes")
-const usuarioRoutes=require("./src/api/usuario/usuario.routes")
-const comentarioRoutes=require("./src/api/comentario/comentario.routes")
+const eventoRoutes = require("./src/api/evento/evento.routes");
+const usuarioRoutes = require("./src/api/usuario/usuario.routes");
+const comentarioRoutes = require("./src/api/comentario/comentario.routes");
 
-server.use("/usuario", usuarioRoutes)
-server.use("/evento", eventoRoutes)
-server.use("/comentario", comentarioRoutes)
+server.use("/usuario", usuarioRoutes);
+server.use("/evento", eventoRoutes);
+server.use("/comentario", comentarioRoutes);
 
-const db=require("./src/utils/db.js")
-db.connectDB()
+const db = require("./src/utils/db.js");
+db.connectDB();
+
+server.use((err, req, res, next) => {
+  return res.status(err.status || 500).json(err.message || "Error inesperado");
+});
+
+server.use("*", (req, res, next) => {
+  return res.status(404).json("Route not found");
+});
+
 server.use("/", (req, res) => {
   res.send("its alive!");
 });
@@ -36,4 +44,3 @@ server.use("/", (req, res) => {
 server.listen(PORT, () => {
   console.log("El server pita en http://localhost:" + PORT);
 });
-
