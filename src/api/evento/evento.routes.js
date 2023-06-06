f891036b8eaef8877e2bd87751c8e8a77b843c10
Miguel <miguelaf71@hotmail.com>
2023-06-06 22:04:05 +0200

    ruta getEventoById añadida

--- src/api/evento/evento.routes.js
+++ src/api/evento/evento.routes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const { isAdmin } = require("../../middleware/auth.js")
 const eventoRoutes = express.Router();
 
-const { getAllEventos,setEvento, deleteEvento, updateEvento } = require('./evento.controller.js');
+const { getAllEventos,setEvento, deleteEvento, updateEvento, getEventoById } = require('./evento.controller.js');
 const { checkEventMandatoryFields } = require('../../middleware/checkfields.js');
 
 eventoRoutes.get('/', getAllEventos);
-eventoRoutes.get("/getbyid/:idEvento",getEventoById)
+eventoRoutes.get("/getbyid/:idEvento", getEventoById)
 eventoRoutes.put('/:idEvento', [isAdmin, checkEventMandatoryFields],updateEvento);
 eventoRoutes.post('/', [isAdmin, checkEventMandatoryFields], setEvento);
 
