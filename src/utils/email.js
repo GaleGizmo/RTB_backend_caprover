3815c0657b13364832b8986507e22c2b4befb600
Miguel <miguelaf71@hotmail.com>
2023-07-07 14:39:29 +0200

    formato del mail de evento

--- src/utils/email.js
+++ src/utils/email.js
@@ -15,8 +15,9 @@ const enviarCorreoElectronico = async (destinatarios, evento) => {
     from: 'rockthebarrio@gmail.com',
     to: 'rockthebarrio@gmail.com',
     bcc: destinatarios,
-    subject: 'Nuevo evento musical',
-    text: `¡Hola! Se ha añadido un nuevo evento musical: ${evento.title}. No te lo pierdas.`,
+    subject: 'Novo evento musical',
+    html: `<p>Ola! Engadiuse un novo evento musical: ${evento.title}.</p>
+    <p>Máis información  <a href="https://rock-the-barrio-front-one.vercel.app/"> aquí.</a></p>`,
   };
 
   try {
