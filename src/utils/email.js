03df571cd517a6fff26daee9d640baac2339a6fa
Miguel <miguelaf71@hotmail.com>
2023-07-07 12:53:44 +0200

    elastic email smtp

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,27 +1,30 @@
 require('dotenv').config();
-const ElasticEmail = require('elasticemail');
+const nodemailer = require('nodemailer');
 
-const elasticemail = new ElasticEmail({
-  apiKey: process.env.API_EMAIL,
-});
+const enviarCorreoElectronico = async (destinatarios, evento) => {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.elasticemail.com',
+    port: 587,
+    auth: {
+      user: process.env.SMTP_USERNAME,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
 
+  const mensaje = {
+    from: 'rockthebarrio@gmail.com',
+    to: 'rockthebarrio@gmail.com',
+    bcc: destinatarios,
+    subject: 'Nuevo evento musical',
+    text: `¡Hola! Se ha añadido un nuevo evento musical: ${evento.title}. No te lo pierdas.`,
+  };
 
-const enviarCorreoElectronico=async(destinatarios, evento)=> {
-    const mensaje = {
-      from: 'RockTheBarrio@gmail.com', 
-      to: 'RockTheBarrio@gmail.com', 
-      bcc: destinatarios, 
-      subject: 'Nuevo evento musical',
-      text: `¡Hola! Se ha añadido un nuevo evento musical: ${evento.title}. No te lo pierdas.`,
-    };
-  
-  
   try {
-    const respuesta = await elasticemail.send(mensaje);
+    const respuesta = await transporter.sendMail(mensaje);
     console.log('Correo electrónico enviado:', respuesta);
   } catch (error) {
     console.error('Error al enviar el correo electrónico:', error);
   }
-}
+};
 
 module.exports = enviarCorreoElectronico;
