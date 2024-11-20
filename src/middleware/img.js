70a0b7779fff77a422756fe66d8b4639d0d4bf52
Miguel <miguelaf71@hotmail.com>
2024-11-20 21:17:32 +0100

    auto resize a width:600 para cloudinary

--- src/middleware/img.js
+++ src/middleware/img.js
@@ -2,13 +2,16 @@ const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("cloudinary").v2;
 
-
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: 'rockthebarrio',
-        allowedFormats: ["jpg", "png", "jpeg", "gif", "pdf"]
-    }
+  cloudinary: cloudinary,
+  params: {
+    folder: "rockthebarrio",
+    allowedFormats: ["jpg", "png", "jpeg", "gif", "pdf"],
+    transformation: [
+      { width: 600, crop: "limit" },
+      { quality: "auto", fetch_format: "auto" },
+    ],
+  },
 });
 
 const upload = multer({ storage });
